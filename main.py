5859eb473f6dcad1b15e11716bd3ea5fa430bba3
qjiujiu <1006485793@qq.com>
2025-11-24 19:41:54 +0800

    feat: 添加posts路由注册

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from app.routers import users, follows
+from app.routers import users, follows, posts
 
 app = FastAPI(title="Forum Management System")
 
 # 注册路由
 app.include_router(users.users_router)
 app.include_router(follows.follows_router)
+app.include_router(posts.posts_router)
 
 
 # uvicorn main:app
