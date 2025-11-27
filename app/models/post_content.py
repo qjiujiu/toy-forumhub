e7834e3e4cb7740273b88d48db999ee815bca10e
qjiujiu <1006485793@qq.com>
2025-11-27 21:17:21 +0800

    帖子内容表

--- app/models/post_content.py
+++ app/models/post_content.py
@@ -1,28 +1,29 @@
-from sqlalchemy import Column, Integer, String, Text
-from sqlalchemy.orm import relationship
-import datetime
+from sqlalchemy import Column, Integer, String, Text, ForeignKey, TIMESTAMP
 import uuid
 from app.models.base import Base
-from sqlalchemy import Enum as SAEnum  # SQLAlchemy 数据类型
-from enum import Enum as PyEnum  # Python 原生枚举
-
+from app.core.time import now_utc8
 
 class PostContent(Base):
     """ 帖子内容表，存储帖子的标题和内容。
 
         CREATE TABLE IF NOT EXISTS post_contents (
-            id INT AUTO_INCREMENT PRIMARY KEY,           -- 系统主键（自增）
-            pcid VARCHAR(36) UNIQUE,                     -- 业务主键PID（UUID）
+            _id INT AUTO_INCREMENT PRIMARY KEY,           -- 系统主键（自增）
+            pcid VARCHAR(36) UNIQUE,                      -- 业务主键PID（UUID）
+            post_id VARCHAR(36) NOT NULL UNIQUE,          -- 帖子 ID (FK -> posts.pid)
             title VARCHAR(255) NOT NULL,                  -- 帖子标题
-            content TEXT NOT NULL                        -- 帖子内容
+            content TEXT NOT NULL,                        -- 帖子内容
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,    -- 创建时间
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,    -- 更新时间
+            FOREIGN KEY (post_id) REFERENCES posts(pid)        -- 外键关联到帖子表
         );
     """
     
     __tablename__ = "post_contents"
 
-    # 系统主键（自增）
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    # 业务主键（UUID，对外使用）
-    pcid = Column(String(36), unique=True, nullable=False, default=lambda: str(uuid.uuid4()))
-    title = Column(String(255), nullable=False)  # 帖子标题
-    content = Column(Text, nullable=False)  # 帖子内容
+    _id = Column(Integer, primary_key=True, autoincrement=True)                                # 系统主键（自增） 
+    pcid = Column(String(36), unique=True, nullable=False, default=lambda: str(uuid.uuid4()))  # 业务主键（UUID，对外使用）
+    post_id =  Column(String(36), ForeignKey("posts.pid"), nullable=False)                     # 帖子ID
+    title = Column(String(255), nullable=False)                                                # 帖子标题
+    content = Column(Text, nullable=False)                                                     # 帖子内容
+    created_at = Column(TIMESTAMP(timezone=True), default=now_utc8)                     # 创建时间
+    updated_at = Column(TIMESTAMP(timezone=True), default=now_utc8, onupdate=now_utc8)  # 更新时间
