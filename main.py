8f26e666f2e613da803efa8d861f56794001cbd1
qjiujiu <1006485793@qq.com>
2026-05-07 14:46:48 +0800

    refact: 接口层调用路径改成api

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-# from app.routers import users, follows, posts, comments, likes
-from app.routers import follows, posts, comments, likes
-from app.routers.v2 import users
+# from app.api import users, follows, posts, comments, likes
+from app.api import follows, posts, comments, likes
+from app.api.v2 import users
 app = FastAPI(title="Forum Management System")
 
 # 注册路由
